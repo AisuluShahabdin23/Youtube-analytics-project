1b341349f1ba66b971d0b519e90aa283008d661d
Aisulu Shahabdin <aisulu_sh_98@mail.ru>
2023-08-10 22:44:01 +0600

    changes for HW2

--- src/channel.py
+++ src/channel.py
@@ -5,7 +5,6 @@ import requests
 from googleapiclient.discovery import build
 
 API_KEY = os.getenv('YOUTUBE_API_KEY')
-youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 
 class Channel:
@@ -13,11 +12,42 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
+        self.__channel_id = channel_id
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        info_channel = json.dumps(channel, indent=2, ensure_ascii=False)
+        info_channel_json = json.loads(info_channel)
+        self.title = info_channel_json['items'][0]['snippet']['title']
+        self.description = info_channel_json['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/channel/' + self.__channel_id
+        self.subs_count = info_channel_json['items'][0]['statistics']['subscriberCount']
+        self.video_count = info_channel_json['items'][0]['statistics']['videoCount']
+        self.view_count = info_channel_json['items'][0]['statistics']['viewCount']
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с API вне класса"""
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
+        return youtube
+
+    @property
+    def channel_id(self):
+        """Свойство для обращения к приватному атрибуту __channel_id"""
+        return self.__channel_id
+
+    def printj(self: dict) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        print(json.dumps(self, indent=2, ensure_ascii=False))
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        url = "https://www.youtube.com/channel/"
-        response = requests.get(url, headers={'id': self.channel_id})
-        data = json.loads(response.text)
-        print(data)
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
+        channel = youtube.channels().list(id=self.__channel_id,  part='snippet,statistics').execute()
+        Channel.printj(channel)
+
+    def to_json(self, file_name: str) -> None:
+        """Запись информации о канале в file_name.json"""
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        with open(os.path.join(file_name), 'w') as file:
+            file.write(json.dumps(channel, indent=2, ensure_ascii=False))

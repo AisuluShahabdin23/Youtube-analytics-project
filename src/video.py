e1ec975e3c496ff537a337e620f274c97678d6fe
Aisulu Shahabdin <aisulu_sh_98@mail.ru>
2023-08-21 21:21:30 +0600

    changes for HW6

--- src/video.py
+++ src/video.py
@@ -4,14 +4,21 @@ from src.channel import Channel
 class Video(Channel):
 
     def __init__(self, video_id):
-        video_response = super().get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                             id=video_id
-                                                             ).execute()
-        self.video_id = video_id
-        self.video_title = video_response['items'][0]['snippet']['title']
-        self.video_url = f"https://youtu.be/{video_response['items'][0]['id']}"
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+        try:
+            video_response = super().get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                                 id=video_id
+                                                                 ).execute()
+            self.video_id = video_id
+            self.title = video_response['items'][0]['snippet']['title']
+            self.video_url = f"https://youtu.be/{video_response['items'][0]['id']}"
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_id = video_id
+            self.title = None
+            self.video_url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.video_title
